888601679a16477718379ec15d46ae472fbbfc8d
klandri <35537931+klandri@users.noreply.github.com>
2020-05-07 21:46:06 +0000

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -3,7 +3,6 @@ const request = require('request')
 nr = "AB123"
 URL = "https://www.samgongustofa.is/umferd/okutaeki/okutaekjaskra/uppfletting?vq=" + nr
 
-
 request(URL, function(err, res, body) {
         let v = body.indexOf("Verksmiðjunr:");
         let t = body.indexOf("Tegund:");
@@ -11,7 +10,7 @@ request(URL, function(err, res, body) {
         argerd = body.slice(a).slice(body.slice(a).indexOf("</sp")-4, (body.slice(a).indexOf("</sp"))).trim()
         let framl = body.slice(t+30, t+50).trim()
         //console.log(framl)
-        let vin = body.slice(v+51, v+68);
+        let vin = body.slice(v+51, v+68); //fyrirmyndar forritunarsnyrtimennska
         let vinURL = "https://vindecoder.eu/check-vin/" + vin
         request(vinURL, function(err, res, body) {
                 let mod = body.indexOf("Model\"")
@@ -20,4 +19,3 @@ request(URL, function(err, res, body) {
                 console.log(nr + " er " + argerd + " " + framl + " " + model)
         });
 });
-
